474a42c60b3aa732e3d4287fe41badced21e1df1
Damian Kilian <kilianpoczta@gmail.com>
2023-03-14 14:19:22 +0100

    pruning trix attachments cmd

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\TrixAttachment;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,18 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('remove-unused-trix-attachments', function () {
+    $unconnected = TrixAttachment::whereNull('place_id')
+        ->where('created_at', '<=', now()->subMonth())
+        ->take(10)
+        ->get();
+    foreach ($unconnected as $key => $value) {
+        if (!Storage::delete('public/' . $value->path) || ($key < 4)) {
+            $unconnected->forget($key);
+        }
+    }
+    $idsToDelete = $unconnected->pluck('id')->all();
+    TrixAttachment::whereIn('id', $idsToDelete)->delete();
+    $this->comment(implode(' ', $idsToDelete));
+})->purpose('Remove unused trix attachments');
